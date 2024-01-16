02a331f45d585a98eee9fa1c1dd064f592304926
Tiberiu Cozan <tibi.cozan@outlook.com>
2024-01-16 16:59:32 +0200

    Create ccwc tool

---
+++ challenge-1-wc-tool/Readme.md
@@ -0,0 +1,20 @@
+## CCWC tool by Coding Challenges
+
+You can find the requirements and description of what the tool needs to do [here](https://codingchallenges.fyi/challenges/challenge-wc/).
+
+### How to use this
+
+#### Method 1: (Python 3)
+Run `python3 ccwc.py -c text.txt`
+
+#### Method 2: (bin)
+
+1. Give the script permissions to execute by running `sudo chmod +x ccwc.py`
+2. Make a hardlink of the script in your bin directory `sudo ln ccwc.py /home/user/usr/local/bin`
+
+ On WSL you can use `sudo ln ccwc.py \\wsl.localhost\Ubuntu\usr\local\bin\ccwc`
+3. Run the command `ccwc -c text.txt`
+
+#### Method 3: (alias)
+Make an alias for the command `alias ccwc='python3 /full/path/to/ccwc.py'
+`
---
+++ challenge-1-wc-tool/ccwc.py
@@ -0,0 +1,72 @@
+import argparse
+import sys
+import locale
+
+def count_bytes_in_text(text):
+    byte_count = len(text.encode('utf-8'))
+    return byte_count
+
+def count_words_in_text(text):
+    word_count = len(text.split())
+    return word_count
+
+def count_lines_in_text(text):
+    line_count = len(text.splitlines())
+    return line_count
+
+def count_characters_in_text(text):
+    # Counting characters in a multibyte-aware way
+    try:
+        char_count = len(text.encode(locale.getpreferredencoding()))
+    except UnicodeEncodeError:
+        # Handle the case where encoding fails
+        char_count = len(text)
+    return char_count
+
+def main():
+    parser = argparse.ArgumentParser(description='Count bytes, words, lines, and characters in a text file or from standard input.')
+    parser.add_argument('-w', action='store_true', help='Count words instead of bytes.')
+    parser.add_argument('-l', action='store_true', help='Count lines instead of bytes.')
+    parser.add_argument('-m', action='store_true', help='Count characters instead of bytes (multibyte-aware).')
+    parser.add_argument('file', metavar='FILE', type=str, nargs='?', help='Specify the path to the text file. If not provided, read from standard input.')
+
+    args = parser.parse_args()
+    file_path = args.file
+
+    # Define a dictionary to map options to functions
+    option_functions = {
+        'w': count_words_in_text,
+        'l': count_lines_in_text,
+        'm': count_characters_in_text,
+        'c': count_bytes_in_text
+    }
+
+    # Check if the user provided any specific options
+    selected_options = [option for option in option_functions if hasattr(args, option) and getattr(args, option)]
+
+    if not selected_options:
+        # If no specific options provided, count all options
+        selected_options = list(option_functions.keys())
+
+    if file_path:
+        # If a file is provided, read from the file
+        with open(file_path, 'r') as file:
+            text = file.read()
+    else:
+        # If no file is provided, read from standard input
+        text = sys.stdin.read()
+
+    # Iterate through selected options and call the corresponding function
+    for option in selected_options:
+        count_function = option_functions.get(option)
+        count = count_function(text)
+        option_name = {
+            'w': 'words',
+            'l': 'lines',
+            'm': 'characters',
+            'c': 'bytes'
+        }[option]
+        print(f"Number of {option_name} in input: {count}")
+
+if __name__ == '__main__':
+    main()
